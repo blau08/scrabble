5f38bfff46ec31574d304b879debf0db62259d9e
Brian Lau <blau08@gmail.com>
2015-08-04 14:12:03 -0700

    Returns a scrabble score for a word

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -10,20 +10,27 @@ class String
   score7 = ["Q", "Z"]
 
   define_method(:scrabble) do
-    if score1.include?(letter)
-      return 1
-    elsif score2.include?(letter)
-      return 2
-    elsif score3.include?(letter)
-      return 3
-    elsif score4.include?(letter)
-      return 4
-    elsif score5.include?(letter)
-      return 5
-    elsif score6.include?(letter)
-      return 8
-    elsif score7.include?(letter)
-      return 10
+      bank = 0
+      self.upcase.split("").each do |i|
+      # binding.pry
+      letter = i
+
+      if score1.include?(letter)
+        bank += 1
+      elsif score2.include?(letter)
+        bank += 2
+      elsif score3.include?(letter)
+        bank += 3
+      elsif score4.include?(letter)
+        bank += 4
+      elsif score5.include?(letter)
+        bank += 5
+      elsif score6.include?(letter)
+        bank += 8
+      elsif score7.include?(letter)
+        bank += 10
+      end
     end
+    return bank
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -9,4 +9,12 @@ describe('String#scrabble') do
   it("returns a scrabble score for a letter") do
     expect("d".scrabble()).to(eq(2))
   end
+
+  it("returns a scrabble score for a word") do
+    expect("dog".scrabble()).to(eq(5))
+  end
+
+  it("returns a scrabble score for a long word") do
+    expect("catdog".scrabble()).to(eq(10))
+  end
 end
